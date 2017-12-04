44386fe0f03ccae5dc6e8c9d7c9eb30f04796f0f
Simon Edvardsson <Simon.Edvards@gmail.com>
2017-12-04 09:05:48 +0100

    Added day 4 to the repo

--- src/2017/dec4.java
+++ src/2017/dec4.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /*
+
 --- Day 4: High-Entropy Passphrases ---
 
 A new system policy has been put in place that requires all accounts to use a passphrase instead of simply a password. A passphrase consists of a series of words (lowercase letters) separated by spaces.
